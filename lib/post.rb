09afc4beeb677278401165724d50946740088aec
Joseph Parker <jparker010190@gmail.com>
2018-05-06 04:27:03 +0000

    automatically backed up by learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,13 +1,10 @@
 class Post
   attr_accessor :title, :author
-  @@song_count = 0
   def initialize (title)
     @title = title
   end
   def author_name
     self.author.name
   end
-  def self.song_count
-      @@song_count
-    end
+
 end
