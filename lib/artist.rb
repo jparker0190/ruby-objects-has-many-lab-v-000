a5b62df1e0ea36860c2b17efa5afe5224bbf653a
Joseph Parker <jparker010190@gmail.com>
2018-05-06 04:27:30 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,5 +1,6 @@
 class Artist
   attr_accessor :name
+  @@song_count = 0
   def initialize (name)
     @name = name
     @songs = []
@@ -7,6 +8,7 @@ class Artist
   def add_song (song)
     @songs << song
     song.artist = self
+    @@song_count += 1
   end
   def songs
     @songs
