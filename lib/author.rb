8dffc83b258dfae9dc8b55441cb16828bc3e4362
Joseph Parker <jparker010190@gmail.com>
2018-05-06 04:30:54 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -17,8 +17,9 @@ class Author
 
   def add_post_by_title(title)
     post = Post.new(title)
-    @posts << post
-    post.title = self
+   @posts << post
+   post.author = self
+   @@post_count += 1
   end
   def self.post_count
     @@post_count
